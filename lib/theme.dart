b9d41e4adbfa679523b627ac363d018a83d71395
Rafid Muhammad <rafid.muhammad@gmail.com>
2022-12-13 13:33:07 +0700

    create theme for entire project

---
+++ lib/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor1 = const Color(0xFF4A80FF);
+Color kPrimaryColor2 = const Color(0xFF0CD1F9);
+Color kAccentColor1 = const Color(0xFF4E4B66);
+Color kAccentColor2 = const Color(0xFF8A8D9F);
+Color kAccentColor3 = const Color(0xFFEFF0F6);
+Color kWhiteColor4 = const Color(0xFFFFFFFF);
+Color kGradientColor1 = const Color(0xFF4A80FF);
+Color kGradientColor2 = const Color(0xFF3462FF);
+
+FontWeight regular = FontWeight.w400;
+FontWeight bold = FontWeight.w700;
+
+TextStyle titleTextStyle = TextStyle(fontWeight: bold, fontSize: 22);
+TextStyle subtitleTextStyle = TextStyle(fontWeight: regular, fontSize: 20);
+TextStyle body1TextStyle = TextStyle(fontWeight: regular, fontSize: 16);
+TextStyle body2TextStyle = TextStyle(fontWeight: regular, fontSize: 14);
